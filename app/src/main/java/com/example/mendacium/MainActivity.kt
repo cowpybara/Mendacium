b71fcde98da80054f697ba46c80adb7c74e330f0
cowpybara <azarodriguez@uamv.edu.ni>
2026-05-01 20:51:24 -0600

    ola inti

--- app/src/main/java/com/example/mendacium/MainActivity.kt
+++ app/src/main/java/com/example/mendacium/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MendaciumTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "ola inti",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
